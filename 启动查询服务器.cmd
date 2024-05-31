485fb36db11595bf3ac41364f4017b0a96c283cb
Soyuz <soyussleet@gmail.com>
2024-06-01 03:27:34 +0800

    查询服务器启动时会直接打开网页

--- 启动查询服务器.cmd
+++ 启动查询服务器.cmd
@@ -1,6 +1,12 @@
- 
+::@echo off
+::在此修改服务器IP:Port以自动启动页面
+set base_url=127.0.0.1
+set base_port=8080
+set full_url=http://%base_url%:%base_port%/mainApp/dbGetAll
+start %full_url%
+:: 启动服务器
 set VENV_PATH=.\DistributeServer\NGA_AutoSave_V3_DistributeServer\NGA_AutoSave_V3_DistributeServer\env1\Scripts  
 call "%VENV_PATH%\activate.bat"  
 cd .\DistributeServer\NGA_AutoSave_V3_DistributeServer\NGA_AutoSave_V3_DistributeServer
-python manage.py runserver 8080
+python manage.py runserver %base_port%
 pause
